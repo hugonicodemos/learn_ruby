4fa6416d1eaefd1ab8a1b34ae7878557abde35c6
Hugo Nicodemos <hugonicodemos@gmail.com>
2015-05-22 13:50:32 -0300

    First two exercises of Learn Ruby The Hard Way.

---
+++ math_exercise.rb
@@ -0,0 +1,17 @@
+# Math Exercise
+puts "\nMath Exercise"
+puts 'I will now count my chickens:'
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "What is 3 + 2? #{3.0 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts 'How about some more.'
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ printing_exercise.rb
@@ -0,0 +1,9 @@
+# Printing Phrases
+puts 'Printing Phrases'
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I Like Typing This.'
+puts 'This is fun.'
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'.'"
+puts "I 'said' do not touch this."
